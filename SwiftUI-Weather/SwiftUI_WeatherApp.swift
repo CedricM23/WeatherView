dd4d3fdfc5b3b881c71ee44975f6c9f41f711763
cedricm23 <Cedricpbc@gmail.com>
2025-08-06 15:01:32 -0400

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Cedric Mentor on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
