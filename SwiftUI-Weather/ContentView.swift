4cd43a0271ff6d35b0082ad6c7436b8bb27c15d9
cedricm23 <Cedricpbc@gmail.com>
2025-08-06 16:38:01 -0400

    added notes

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -97,6 +97,11 @@ struct BackgroundView: View {
         
         LinearGradient(colors: [isNight ? .black : .blue, isNight ? . gray : Color("lightBlue")], startPoint: .topLeading, endPoint: .bottomTrailing)
             .edgesIgnoringSafeArea(.all)
+    
+//        ContainerRelativeShape()
+//            .fill(isNight ? Color.black.gradient : Color.blue.gradient)
+//            .ignoresSafeArea()
+//    
     }
 }
 
